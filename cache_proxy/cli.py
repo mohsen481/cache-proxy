e4bda4f622001bcd6ba6faeb588d87c364d18861
mohsen <m16192494>
2025-12-30 04:44:25 -0800

    Implement proxy server functionality

--- cache_proxy/cli.py
+++ cache_proxy/cli.py
@@ -2,6 +2,8 @@ import typer
 import uvicorn
 from typing import Annotated
 from .main import app
+
+
 cli_app = typer.Typer()
 
 origin_option = Annotated[str, typer.Option(help="target server for forwarding requests")]
@@ -10,6 +12,8 @@ port_option = Annotated[int, typer.Option()]
 
 @cli_app.command()
 def run_proxy_server(origin: origin_option, port: port_option = 2000):
-    app.state.origin = origin #set origin in app state so fastapi functions can use it
-    uvicorn.run("cache_proxy.main:app", port=port, reload=False)
+    #store "origin" and "port" in app state so that fastapi functions can use.
+    app.state.origin=origin 
+    app.state.port=port
+    uvicorn.run(app,host='0.0.0.0',port=port, reload=False)
 
--- cache_proxy/main.py
+++ cache_proxy/main.py
@@ -1,9 +1,88 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI,Request,Response
+import httpx
+import redis
+from urllib.parse import urlparse
 
 app = FastAPI()
 
-@app.get('/')
-def show_origin(request: Request):
-    origin = getattr(app.state, "origin", "not set")
-    return {origin}
+# r=redis.Redis(decode_responses=True)
 
+hop_by_hop_headers=[ #headers that should not be forwarded by proxie according to RFC 2616.
+
+    'Connection',
+    'Keep-Alive',
+    'Proxy-Authenticate',
+    'Proxy-Authorization',
+    'TE',
+    'Trailers',
+    'Transfer-Encoding',
+    'Upgrade',
+]
+
+hop_by_hop_headers=[i.lower() for i in hop_by_hop_headers]
+
+
+@app.api_route('/{path:path}',methods=['GET','POST','PUT','DELETE'])
+
+async def forward_to_origin(path,request:Request):
+        origin=request.app.state.origin
+        clean_path = path if path.startswith('/') else f'/{path}'
+        url=f"{origin}{clean_path}"
+        request_headers=dict(request.headers)
+        #remove hop-by-hop headers in request before forwarding to origin server
+        modified_request_headers={
+            k:v for k,v in request_headers.items() if k.lower() not in hop_by_hop_headers
+            and k.lower()!='accept-encoding'
+            }
+        modified_request_headers['host']=urlparse(origin).netloc
+        body=await request.body()
+        #forward the request to origin server
+        async with httpx.AsyncClient(follow_redirects=True) as client:
+            response =await client.request(
+                method=request.method,
+                url=url,
+                headers=modified_request_headers,
+                params=dict(request.query_params),
+                content=body,
+                timeout=None
+                )
+        def replace_url():
+            """
+             Replace origin URL with proxy URL in text-based responses
+             to ensure links work through the proxy.
+            """
+            try:
+                content_=response.content
+                content_type=response.headers['content-type']
+                if 'text'in content_type or 'javascript' in content_type:
+                    text_content=content_.decode('utf8')
+                    port=request.app.state.port
+                    proxy_url=f"http://127.0.0.1:{port}"
+                    text_content=text_content.replace(origin,proxy_url)
+                    content_=text_content.encode('utf8')
+                    return content_
+            except:
+                return response.content
+            
+        final_content=replace_url()
+        if final_content is None:
+            final_content=response.content
+
+
+        response_headers=dict(response.headers)
+        #remove hop-by-hop headers in response before forwarding to client
+        modified_response_headers={
+            k:v for k,v in response_headers.items() if k.lower() not in hop_by_hop_headers
+            and k.lower() not in ['content-encoding','content-length']
+            }
+
+        
+        modified_response_headers["X-CACHE"]="MISS"
+        modified_response_headers["content-length"]=str(len(final_content))
+        
+
+        return Response(
+            content=final_content,
+            headers=modified_response_headers,
+            status_code=response.status_code
+            )
